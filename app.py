c3f08e0dbb1f340a730b89a75520d85b0c10d950
Pascal Ghaname <pascaljunior49@gmail.com>
2024-03-10 22:50:33 +0100

    wip

--- app.py
+++ app.py
@@ -4,14 +4,23 @@ import requests
 import numpy as np
 from PIL import Image
 
+st.set_page_config(
+    page_title = 'SMN',
+    page_icon = ':lower_left_paintbrush:',
+    initial_sidebar_state= 'auto'
+)
 
-st.markdown("<h1 style='text-align: center; color: Crimson;'>Say My Name</h1>", unsafe_allow_html=True)
-st.markdown("<h5 style='text-align: center; color: grey;'>Classify yo Painting</h5>", unsafe_allow_html=True)
-st.markdown("<h4 style='text-align: left; color: IndianRed;'>Upload yo Painting</h4>", unsafe_allow_html=True)
+with st.sidebar:
+    st.title("'Say my name'")
+    st.image('smn.png')
+
+st.markdown("<h1 style='text-align: center; color: Crimson;'>SMN</h1>", unsafe_allow_html=True)
+st.markdown("<h5 style='text-align: center; color: IndianRed;'>Painting's movement classifier</h5>", unsafe_allow_html=True)
 
 
 painting = st.file_uploader(
-    label = ':lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush:',
+    label = ':lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush:\
+            :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush:',
     type = ['png', 'jpg'],
     accept_multiple_files = False,
     help = ':middle_finger:',
@@ -19,15 +28,14 @@ painting = st.file_uploader(
 
 if painting is not None:
     r = requests.post(url = 'http://localhost:8000/predict?', files = {'file':painting})
-    st.write(r)
     response = r.json()
-    st.write('This is my response:')
-    st.markdown(response)
+    my_pred = response['mypred'][1].upper()
 
+    st.write(f"Paiting's movement : **{my_pred}**")
 
-    st.write('----------This is testing from now on------------')
     image = Image.open(painting)
     img_array = np.array(image)
+    st.balloons()
 
     if image is not None:
         st.image(
