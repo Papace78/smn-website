770c9d5c416082d1ff5e7f3ad6012d7014f4ec97
Pascal Ghaname <pascaljunior49@gmail.com>
2024-03-12 16:59:12 +0100

    wip

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
 st.set_page_config(
     page_title = 'SMN',
@@ -26,6 +27,7 @@ painting = st.file_uploader(
     help = ':middle_finger:',
 )
 
+
 if painting is not None:
     r = requests.post(url = 'http://localhost:8000/predict?', files = {'file':painting})
     response = r.json()
@@ -43,3 +45,34 @@ if painting is not None:
             caption=f"You amazing image has shape {img_array.shape[0:2]}",
             use_column_width=True,
         )
+
+
+
+# Section 1
+st.title('My Presentation')
+
+# Section 2
+st.header('Introduction')
+st.write('This is an introduction to my presentation.')
+image = plt.imread('presentation/La_Junta_de_Filipinas-romanticism.jpg')
+st.image(image)
+
+# Section 3
+st.header('Main Content')
+st.write('This is the main content of my presentation.')
+st.image(plt.imread('presentation/Iwan_Iwanowitsch_Schischkin_001-realism.jpg'))
+
+# Section 4
+st.header('Conclusion')
+st.write('This is the conclusion.')
+
+# Section 5
+st.header('References')
+st.write('Here are some references.')
+
+# Section 6
+st.write('This is more content to make the page longer.')
+st.write('This is more content to make the page longer.')
+st.write('This is more content to make the page longer.')
+st.write('This is more content to make the page longer.')
+st.write('This is more content to make the page longer.')
