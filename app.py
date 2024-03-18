a6c7685d18b2ee798d926ea3dffda4bb1e0fc25c
Pascal Ghaname <pascaljunior49@gmail.com>
2024-03-18 11:16:38 +0100

    camera access

--- app.py
+++ app.py
@@ -17,17 +17,38 @@ with st.sidebar:
 st.markdown("<h1 style='text-align: center; color: Crimson;'>SMN</h1>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center; color: IndianRed;'>Painting's movement classifier</h5>", unsafe_allow_html=True)
 
-painting = st.camera_input(
-    label = "Take a picture or upload a saved painting",
-    help = ':middle_finger:'
-)
+
 
 painting = st.file_uploader(
     label = ':lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush:\
-            :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush:',
+             Upload a saved painting :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush:',
     type = ['png', 'jpg'],
     accept_multiple_files = False,
     help = ':middle_finger:',
+)
+
+if painting is not None:
+    r = requests.post(url = 'https://smndocker-rdwmjilicq-ew.a.run.app/predict?', files = {'file':painting})
+    response = r.json()
+    my_pred = response['mypred'][1].upper()
+
+    st.write(f"Paiting's movement : **{my_pred}**")
+
+    image = Image.open(painting)
+    img_array = np.array(image)
+    st.balloons()
+
+    if image is not None:
+        st.image(
+            image,
+            caption=f"You amazing image has shape {img_array.shape[0:2]}",
+            use_column_width=True,
+        )
+
+painting = st.camera_input(
+    label = ":lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush:\
+             Take a picture of a painting :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush: :lower_left_paintbrush:",
+    help = ':middle_finger:'
 )
 
 if painting is not None:
